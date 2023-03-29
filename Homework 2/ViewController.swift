5727c682ce80256c3affe1bfe3fa86050b73b5aa
Cody Scherle <c.scherle@setonhill.edu>
2023-03-29 18:47:38 -0400

    Homework 2

--- Homework 2/ViewController.swift
+++ Homework 2/ViewController.swift
@@ -9,6 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Animal1: UITextField!
+    
+    @IBOutlet weak var Animal2: UITextField!
+    
+    @IBOutlet weak var Color1: UITextField!
+    
+    @IBOutlet weak var Color2: UITextField!
+    
+    @IBOutlet weak var Sport:   UITextField!
+    
+    @IBOutlet weak var MadLib: UILabel!
+    @IBAction func CreateMadLib(_ sender: UIButton) {
+        MadLib.text = "The " + Color1.text! + " " + Animal1.text! + " played " + Sport.text! + " against the " + Color2.text! + " " + Animal2.text! + " and won!"
+    
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
